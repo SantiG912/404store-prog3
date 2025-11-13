ad70224cdadb4f83316fed6e0fe74d2ae790c469
SantiG912 <santiagoagrandoli03@gmail.com>
2025-11-12 23:23:35 -0300

    cambios minimos en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import Products from './components/Products';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Index from './components/Index';
+import Darkmode from './components/Darkmode';
 export default function App() {
   return (
     <>
     <BrowserRouter>
       <Navbar/>
-      <Routes>
-        <Route path="/Products" element={<Products/>}/>
-      </Routes>
+
+      <section className="main-section">
+        <section className="main-content">
+          <Routes>
+            <Route path="/" element={<Index/>}/>
+            <Route path="/Products" element={<Products/>}/>
+          </Routes>
+        </section>
+      </section>
+
       <Footer/>
     </BrowserRouter>
     </>
