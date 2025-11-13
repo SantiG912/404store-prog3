a2dce761e1f4f9ef3fda47990e3386e8c9a102e5
SantiG912 <santiagoagrandoli03@gmail.com>
2025-11-12 23:18:09 -0300

    useFetch mejorado, se agregaron estados

--- src/api/useFetch.js
+++ src/api/useFetch.js
@@ -2,12 +2,37 @@ import React, { useEffect, useState } from 'react'
 
 export default function useFetch(url) {
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
-        .then((response) => response.json())
-        .then((data) => setData(data))
-    }, [])
+        if(!url) return;
 
-    return {data};
+        const fetchData = async () => {
+            try{
+                setLoading(true);
+                setError(null);
+                const response = await fetch(url);
+
+                if(!response.ok){
+                    throw new Error(`Error ${response.status}: ${response.statusText}`);
+                }
+
+                const data = await response.json();
+                setData(data);
+
+            }catch(err){
+
+                if(err.name !== "AbortError"){
+                    setError(err);
+                }
+
+            }finally{
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [url])
+
+    return {data, loading, error};
 }
