805641cae8fc2046d977e251383acea471c81c71
SantiG912 <santiagoagrandoli03@gmail.com>
2025-11-12 23:21:03 -0300

    empecé a trabajar en el footer, navbar terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faHouse, faStore} from '@fortawesome/free-solid-svg-icons'
+import {faHouse, faStore, faGhost} from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Darkmode from './Darkmode';
 
 export default function Navbar() {
   return (
@@ -10,25 +11,24 @@ export default function Navbar() {
         <section className="header-content">
           <section className="header-icon">
             <Link className="header-links" to="/">
-              <img src={null} alt="404store Icon" />
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M40.1 467.1l-11.2 9C25.7 478.6 21.8 480 17.8 480 8 480 0 472 0 462.2L0 192C0 86 86 0 192 0S384 86 384 192l0 270.2c0 9.8-8 17.8-17.8 17.8-4 0-7.9-1.4-11.1-3.9l-11.2-9c-13.4-10.7-32.8-9-44.1 3.9L269.3 506c-3.3 3.8-8.2 6-13.3 6s-9.9-2.2-13.3-6l-26.6-30.5c-12.7-14.6-35.4-14.6-48.2 0L141.3 506c-3.3 3.8-8.2 6-13.3 6s-9.9-2.2-13.3-6L84.2 471c-11.3-12.9-30.7-14.6-44.1-3.9zM160 192a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm96 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>
               <h3>404 Store</h3>
             </Link>
-            <section className="header-menu-container">
-              <ul className="header-menu">
-                <li>
-                  <Link className="header-links" to="/">
-                    <FontAwesomeIcon icon={faHouse}/>
-                    Inicio
-                  </Link>
-                </li>
-                <li>
-                  <Link className="header-links" to="/Products">
-                    <FontAwesomeIcon icon={faStore}/>
-                    Productos
-                    </Link>
-                </li>
-              </ul>
-            </section>
+          </section>
+          <section className="darkmode-container">
+            <Darkmode/>
+          </section>
+          <section className="header-menu-container">
+            <nav className="header-menu">
+              <Link className="header-links" to="/">
+                <FontAwesomeIcon icon={faHouse}/>
+                Inicio
+              </Link>
+              <Link className="header-links" to="/Products">
+                <FontAwesomeIcon icon={faStore}/>
+                Productos
+              </Link>
+            </nav>
           </section>
         </section>
       </section>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import useFetch from '../api/useFetch';
 const {VITE_API_URL: url} = import.meta.env;
 export default function Products() {
-  const {data} = useFetch(url);
+  const {data, loading, error} = useFetch(url);
   return (
     <>
     <section className="products-container">
       <h3>Productos</h3>
       <ul>
+        {error && <li>Error: {error}</li>}
+        {loading && <li>Cargando datos...</li>}
         {data?.map((product) => {
           return <li key={product.id}>{product.title}</li>
         })}
